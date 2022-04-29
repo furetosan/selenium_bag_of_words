25f96318deec29e976e7a375164553e17c8fdcbe
furetosan <furetosan@gmail.com>
2022-04-29 18:08:40 -0300

    proceeding to analysis

--- README.md
+++ README.md
@@ -3,3 +3,13 @@
 This is a simple script that runs chromedriver through the first page of google results for a given query
 
 - mateste.py reads from google and prints to console
+
+## Requirements
+
+In this particular implementation I used Chrome, so you'll need chromedriver available on the path.
+
+## To-Dos
+
+Possibly use nltk in order to:
+
+- tokenizing, stemming, PoS tagging...
--- mateste.py
+++ mateste.py
@@ -12,7 +12,7 @@ chrom = webdriver.Chrome()
 # /html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input
 # /html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input
 
-chrom.get('http://google.com/search?q=o+que+é+website')
+chrom.get('http://google.com/search?q=matematica')
 
 # 1. acima - escolher uma forma de pegar os resultados da busca
 # 2. guardar textos e links de cada resultado para visita posterior
@@ -20,21 +20,34 @@ chrom.get('http://google.com/search?q=o+que+é+website')
 #   cada pagina dos resultados (por enquanto da primeira pagina)
 
 # //*[@id="rso"]/div[7]/div/div/div[1]
-elementos = chrom.find_elements(by=By.XPATH,value='//*[@id="rso"]/*/div/*/div/a')
+elementos_primeira_pagina = chrom.find_elements(by=By.XPATH,value='//*[@id="rso"]/*/div/*/div/a')
+
+# 
+
+links_proximas_paginas = chrom.find_elements(by=By.XPATH, value='//*[@id="xjs"]/table/tbody/tr/td[*]/a')
 
 # rso = results search organic?
 
-lista_de_sites = []
-for i in range(len(elementos)):
-    print(elementos[i].text)
-    print(elementos[i].get_attribute('href'))
+lista_de_sites_primeira_pagina = []
+for i in range(len(elementos_primeira_pagina)):
+    print(elementos_primeira_pagina[i].text)
+    print(elementos_primeira_pagina[i].get_attribute('href'))
     site_a_visitar = {}
-    site_a_visitar['text'] = elementos[i].text
-    site_a_visitar['href'] = elementos[i].get_attribute('href')
-    lista_de_sites.append(site_a_visitar)
+    site_a_visitar['text'] = elementos_primeira_pagina[i].text
+    site_a_visitar['href'] = elementos_primeira_pagina[i].get_attribute('href')
+    lista_de_sites_primeira_pagina.append(site_a_visitar)
+
+# lista_de_sites = []
+# for i in range(len(elementos)):
+#     print(elementos[i].text)
+#     print(elementos[i].get_attribute('href'))
+#     site_a_visitar = {}
+#     site_a_visitar['text'] = elementos[i].text
+#     site_a_visitar['href'] = elementos[i].get_attribute('href')
+#     lista_de_sites.append(site_a_visitar)
 
 textos = []
-for s in lista_de_sites:
+for s in lista_de_sites_primeira_pagina:
     chrom.get(s['href'])
     texto = chrom.find_element(by=By.XPATH,value="/html/body").text
     print(texto)
